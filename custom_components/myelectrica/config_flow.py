a4e8b79c5350194fc829fcb4dea04ef65fa5969c
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2025-01-10 02:39:58 +0200

    Update config_flow.py

--- custom_components/myelectrica/config_flow.py
+++ custom_components/myelectrica/config_flow.py
@@ -1,18 +1,19 @@
-"""ConfigFlow și OptionsFlow pentru integrarea MyElectrica România."""
+"""
+ConfigFlow și OptionsFlow pentru integrarea MyElectrica România.
+"""
 
 import logging
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from .__init__ import _fetch_login
+
+from .api import MyElectricaAPI
 from .const import DOMAIN, DEFAULT_UPDATE
 
 _LOGGER = logging.getLogger(__name__)
 
-
 class MyElectricaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Gestionarea ConfigFlow pentru integrarea MyElectrica."""
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -26,19 +27,21 @@ class MyElectricaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             cod_client = user_input["cod_client"]
             cod_nlc = user_input["cod_nlc"]
 
-            # Completați automat cod_incasare cu 00 dacă este mai scurt de 12 caractere
+            # Completează automat cod_incasare la 12 caractere
             if len(cod_incasare) < 12:
                 cod_incasare = cod_incasare.zfill(12)
                 _LOGGER.debug("Codul de încasare completat automat: %s", cod_incasare)
 
-            _LOGGER.debug("Date introduse: username=%s, cod_incasare=%s, cod_client=%s, cod_nlc=%s",
-                          username, cod_incasare, cod_client, cod_nlc)
-
-            # Validăm autentificarea
-            auth_response = await _fetch_login(self.hass, username, password)
-
-            if auth_response:
-                _LOGGER.debug("Autentificare reușită!")
+            # Validăm autentificarea prin API (simplu)
+            api = MyElectricaAPI(
+                self.hass,
+                username=username,
+                password=password,
+                cod_incasare=cod_incasare,
+                cod_nlc=cod_nlc
+            )
+            if await api.async_login():
+                # OK -> Creăm intrarea
                 return self.async_create_entry(
                     title=f"MyElectrica ({cod_incasare})",
                     data={
@@ -54,7 +57,7 @@ class MyElectricaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "auth_failed"
                 _LOGGER.error("Autentificare eșuată pentru utilizatorul %s", username)
 
-        # Schema formularului de configurare
+        # Schema formularului
         data_schema = vol.Schema(
             {
                 vol.Required("username"): str,
@@ -65,7 +68,6 @@ class MyElectricaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional("update_interval", default=DEFAULT_UPDATE): int,
             }
         )
-
         return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
 
     @staticmethod
@@ -88,14 +90,9 @@ class MyElectricaOptionsFlow(config_entries.OptionsFlow):
 
         if user_input is not None:
             cod_incasare = user_input["cod_incasare"]
-
-            # Completați automat cod_incasare cu 00 dacă este mai scurt de 12 caractere
             if len(cod_incasare) < 12:
                 cod_incasare = cod_incasare.zfill(12)
-                _LOGGER.debug("Codul de încasare completat automat: %s", cod_incasare)
 
-            # Salvăm noile date în config_entry
-            _LOGGER.debug("Modificare date: %s", user_input)
             self.hass.config_entries.async_update_entry(
                 self.config_entry,
                 options={
@@ -111,7 +108,7 @@ class MyElectricaOptionsFlow(config_entries.OptionsFlow):
             )
             return self.async_create_entry(title="", data={})
 
-        # Schema pentru modificarea opțiunilor
+        # Construim schema de opțiuni
         options_schema = vol.Schema(
             {
                 vol.Optional("username", default=self.config_entry.data.get("username", "")): str,
@@ -119,7 +116,7 @@ class MyElectricaOptionsFlow(config_entries.OptionsFlow):
                 vol.Optional("cod_incasare", default=self.config_entry.data.get("cod_incasare", "")): str,
                 vol.Optional("cod_client", default=self.config_entry.data.get("cod_client", "")): str,
                 vol.Optional("cod_nlc", default=self.config_entry.data.get("cod_nlc", "")): str,
-                vol.Optional("update_interval", default=self.config_entry.options.get("update_interval", DEFAULT_UPDATE)): int,
+                vol.Optional("update_interval", default=self.config_entry.data.get("update_interval", DEFAULT_UPDATE)): int,
             }
         )
 
